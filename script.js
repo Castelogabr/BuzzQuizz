390f0de7f256c142689ace2eb0f6668bc6f380ea
Gustavo Henrique <binarymaster100@gmail.com>
2022-08-31 23:00:21 -0300

    Bug Resolvido

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const requestAPI = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/qu
 requestAPI.then(
   carregarQuizes
 )
-requestAPI.cath(
-  falhaAoCarregarQuizes
-)
+
 
 function carregarQuizes(resposta){
 
@@ -12,11 +10,16 @@ function carregarQuizes(resposta){
   for(i=0; i < 6; i++){
     element_todos_quizes.innerHTML += `
     <div class="quiz">
-      <h2>${resposta.data[i].title}<h2>
+      <h2>${resposta.data[i].title}</h2>
       <img src="${resposta.data[i].image}">
     <div>
     `
   }
+  const quiz_elements = document.querySelectorAll("div .quiz")
+  quiz_elements.forEach(array => {
+    array.addEventListener("click", () => alert("click"))
+  })
+  console.log(quiz_elements)
 }
 
 function falhaAoCarregarQuizes(resposta){
