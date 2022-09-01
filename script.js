c44e1b8ea36b92329c622423c185823f2282fafa
Gustavo Henrique <binarymaster100@gmail.com>
2022-08-31 21:50:14 -0300

    Bug a resolver

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+const requestAPI = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+requestAPI.then(
+  carregarQuizes
+)
+requestAPI.cath(
+  falhaAoCarregarQuizes
+)
 
-function RenderizarMsg() {
-    let Renderizar = document.querySelector("main .header");
-    Renderizar.innerHTML = `
-    
-    `;
+function carregarQuizes(resposta){
+
+  const element_todos_quizes = document.querySelector(".todos-quizz")
+  for(i=0; i < 6; i++){
+    element_todos_quizes.innerHTML += `
+    <div class="quiz">
+      <h2>${resposta.data[i].title}<h2>
+      <img src="${resposta.data[i].image}">
+    <div>
+    `
   }
+}
+
+function falhaAoCarregarQuizes(resposta){
+
+}
